a343aa261f2c82fe8cd95d037e9c5ad447f89ded
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-07-06 01:07:59 +0300

    Добавляет Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,15 +21,36 @@ const makeGallery = galleryItems.map(({ preview, original, description}) => {
 
 gallery.insertAdjacentHTML("beforeend", makeGallery);
 
-const galleryLi = document.querySelector('.gallery__link');
+console.log()
 // запрет перехода по ссылке
 
-function onBlock(event) {
-    event.preventDefault();
-}
-galleryLi.addEventListener('click', onBlock);
+// const galleryLi = document.querySelector('.gallery__link');
+
+// function onBlock(event) {
+//     event.preventDefault();
+// }
+// galleryLi.addEventListener('click', onBlock);
 
 // Реализация делегирования на div.gallery и получение url большого изображения.
+function openModal(event) {
+    event.preventDefault();
+if (!event.target.classList.contains('gallery__image')) {
+    return;
+}
+    const bigImg = event.target.dataset.source;
+    const instans = basicLightbox.create(`<img src="${bigImg}">`)
+
+    instans.show();
+    // console.log(bigImg);
+}
+gallery.addEventListener('click', openModal)
+
 // Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+
+// const winModal = basicLightbox.create(`
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="800" height="600">
+// `)
+
+// winModal.show()
 // Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
 // Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
