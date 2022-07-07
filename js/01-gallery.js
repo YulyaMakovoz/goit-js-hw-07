b1a41a701659a782163bb4b0619264ed5316bcc5
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-07-07 22:30:40 +0300

    Добавляет Task-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,36 +21,40 @@ const makeGallery = galleryItems.map(({ preview, original, description}) => {
 
 gallery.insertAdjacentHTML("beforeend", makeGallery);
 
-console.log()
-// запрет перехода по ссылке
-
-// const galleryLi = document.querySelector('.gallery__link');
-
-// function onBlock(event) {
-//     event.preventDefault();
-// }
-// galleryLi.addEventListener('click', onBlock);
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+    gallery.addEventListener('click', openModal)
 
 // Реализация делегирования на div.gallery и получение url большого изображения.
+
 function openModal(event) {
+    // запрет перехода по ссылке
     event.preventDefault();
-if (!event.target.classList.contains('gallery__image')) {
-    return;
-}
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
     const bigImg = event.target.dataset.source;
-    const instans = basicLightbox.create(`<img src="${bigImg}">`)
+    // Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+    const instance = basicLightbox.create(`<img src="${bigImg}">`);
 
-    instans.show();
-    // console.log(bigImg);
+    instance.show();
+    window.addEventListener('keydown', onEscPress);
+    
+
+    // Закрытие по escape
+
+    function closeModal() {
+        window.removeEventListener('keydown', onEscPress);
+    
+        instance.close();
+    }
+
+    function onEscPress(event) {
+        if (event.code === 'Escape') {
+            closeModal();
+        
+        }
+    
+    }
 }
-gallery.addEventListener('click', openModal)
-
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-
-// const winModal = basicLightbox.create(`
-//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="800" height="600">
-// `)
-
-// winModal.show()
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+  // Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+    // в html 
